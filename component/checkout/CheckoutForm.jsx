d939c1a22dfe57c21cfc620a1de1ff66ee3b864f
jricramc <jricramc@mit.edu>
2024-01-08 12:56:08 -0500

    new

--- component/checkout/CheckoutForm.jsx
+++ component/checkout/CheckoutForm.jsx
@@ -85,11 +85,11 @@ export default function CheckoutForm() {
     <form id="payment-form" onSubmit={handleSubmit}>
 
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id="submit">
+      {/* <button disabled={isLoading || !stripe || !elements} id="submit">
         <span id="button-text">
           {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
         </span>
-      </button>
+      </button> */}
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
     </form>
